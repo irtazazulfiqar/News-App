8f2cac799a30d9df3b637406886e391a4ce5eae6
irtazazulfiqar <irtaza.zulfiqar@codefulcrum.com>
2024-08-19 22:06:05 +0500

    Fix Bug

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
-    path('signin/', LoginUserView.as_view(), name='register'),
+    path('signin/', LoginUserView.as_view(), name='signin'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view, name='token_refresh'),
 ]
