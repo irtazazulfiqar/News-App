8be0a1528eed072d584aa5414232319045c85a7a
irtazazulfiqar <irtaza.zulfiqar@codefulcrum.com>
2024-08-19 19:40:46 +0500

    Create Signin user interface and API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         {/* We can add other routes here */}
       </Routes>
