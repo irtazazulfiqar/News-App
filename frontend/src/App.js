e5334debda22032ab722929469288ae212302741
irtazazulfiqar <irtaza.zulfiqar@codefulcrum.com>
2024-08-28 15:46:07 +0500

    Remove signin files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import SignUp from 'pages/SignUp';
-import SignIn from 'pages/SignIn';
 import Dashboard from 'pages/Dashboard';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <Router>
         <Header />
         <Routes>
-          <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
         <Footer />
