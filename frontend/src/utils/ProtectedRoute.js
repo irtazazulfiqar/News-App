0b440255d07d91cb4e3337cf3089800691ad2abc
irtazazulfiqar <irtaza.zulfiqar@codefulcrum.com>
2024-08-29 12:54:11 +0500

    Try merge

--- frontend/src/utils/ProtectedRoute.js
+++ frontend/src/utils/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from 'context/AuthContext';
 
 function ProtectedRoute({ element, ...rest }) {
   const { isAuthenticated } = useAuth();
