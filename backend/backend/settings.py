d5c87fc49b97b4b017f11e1804117f517d90778d
irtazazulfiqar <irtaza.zulfiqar@codefulcrum.com>
2024-09-06 12:23:27 +0500

    Remove Bugs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ CORS_ALLOW_METHODS = [
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
